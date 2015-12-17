69eeb0038b58bc4b7aff462fc89bfaab9f4a92e1
Nick Jaremek <nick13jaremek@gmail.com>
2015-12-17 15:33:18 +0100

    Added action creators and refactored tests.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -8,6 +8,8 @@ import Router, {Route} from 'react-router';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from '../src/reducer';
+import {setState} from './action_creators';
+import io from 'socket.io-client';
 import App from './components/App';
 import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Results';
@@ -24,6 +26,11 @@ store.dispatch({
     }
   }
 });
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state =>
+  store.dispatch(setState(state))
+);
 
 const routes = <Route component={App}>
   <Route path="/results" component={ResultsContainer} />
